db37604a322253771ccea11b76540c0951804eb3
Christophe Gagnier <admin@cgagnier.ca>
2015-04-29 16:50:03 -0400

    Add start of UnitOfWork (Incomplete)

---
+++ photo_hdr_duval/photo_hdr_duval/DAL/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace photo_hdr_duval.DAL
+{
+    interface IUnitOfWork : IDisposable
+    {
+
+
+        void Save();
+    }
+}
--- photo_hdr_duval/photo_hdr_duval/DAL/UnitOfWork.cs
+++ photo_hdr_duval/photo_hdr_duval/DAL/UnitOfWork.cs
@@ -5,7 +5,37 @@ using System.Web;
 
 namespace photo_hdr_duval.DAL
 {
-	public class UnitOfWork
+	public class UnitOfWork : IUnitOfWork
 	{
+
+		public int CurrentUserID { get { return 1; } }
+
+		//private Statistiques_SkiContext context = new Statistiques_SkiContext();
+
+        
+
+		public void Save()
+		{
+			//context.SaveChanges();
+		}
+
+		private bool disposed = false;
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					//context.Dispose();
+				}
+			}
+			this.disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
 	}
 }
