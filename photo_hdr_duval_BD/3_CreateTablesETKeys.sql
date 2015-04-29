bc9ca9b7729a275a97754098e370467239ae3b6c
tigie11 <1369599@LLBD0605INFO08.laboratoire.collegeem.qc.ca>
2015-04-29 16:36:43 -0400

    Some modification in CreateTablesETKeys

--- photo_hdr_duval_BD/3_CreateTablesETKeys.sql
+++ photo_hdr_duval_BD/3_CreateTablesETKeys.sql
@@ -7,10 +7,13 @@ CREATE TABLE RDV.RDVs(
 	RDVID int NOT NULL IDENTITY,
 	DateRDV DateTime Null,
 	HeureRDV time NULL,
-	Commentaire nvarchar(4000) NULL,
-	NomPrenomProprio nvarchar(70) NOT NULL,
-	TelProprietaire nvarchar(10) NOT NULL,
+	Commentaire nvarchar(MAX) NULL,
+	NomPrenomProprietaire nvarchar(70) NOT NULL,
+	TelPrincipalProprietaire nvarchar(10) NOT NULL,
+	TelCellProprietaire nvarchar(10) NOT NULL,
 	AdressePropriete nvarchar(70) NOT NULL,
 	EmailProprietaire nvarchar(30) NULL
+
+	PRIMARY KEY (RDVID)
 ) ON [PRIMARY];
 GO
