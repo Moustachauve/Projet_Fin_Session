12567f9016f1bd3b1a0a6563d6785e628b1af8f3
IceSentry <c.giguere42@gmail.com>
2015-05-04 09:59:03 -0400

    Add RDVRepository and remove RDVController

---
+++ photo_hdr_duval/photo_hdr_duval/DAL/RDVRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace photo_hdr_duval.DAL
+{
+	public class RDVRepository : Repository<RDVRepository>
+	{
+		/*public RDVRepository(context): base(context)
+		{}*/
+
+
+
+	}
+}
