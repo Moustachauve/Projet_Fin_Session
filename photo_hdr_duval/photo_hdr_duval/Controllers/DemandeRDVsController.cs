e37e7b51470e43feec21420d1301bbff3e6fe391
BobWithHair <matt20four@gmail.com>
2015-05-06 16:44:05 -0400

    Revert "Revert "fix""
    This reverts commit 8253720cd0c42b9912dafff7d91c73b8df8d3b54.

--- photo_hdr_duval/photo_hdr_duval/Controllers/DemandeRDVsController.cs
+++ photo_hdr_duval/photo_hdr_duval/Controllers/DemandeRDVsController.cs
@@ -16,9 +16,19 @@ namespace photo_hdr_duval.Controllers
         private UnitOfWork uow = new UnitOfWork();
 
         // GET: DemandeRDVs
-        public ActionResult Index()
+        public ActionResult Index(string sortString, bool? asc)
         {
-            return View(uow.RDVRepository.Get());
+            IEnumerable<RDV> rdvs = null;
+
+            ViewBag.isAsc = asc;
+            ViewBag.orderBy = sortString;
+
+            if (sortString == null)
+                rdvs = uow.RDVRepository.Get();
+            else
+                rdvs = uow.RDVRepository.GetOrderBy(sortString, asc != null ? (bool)asc : false);
+
+            return View(rdvs.ToList());
         }
 
         // GET: DemandeRDVs/Details/5
