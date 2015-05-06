0cd7e62906a57ecbec6df7501b9d9c62625615c8
tigie11 <1369599@LLBD0605INFO08.laboratoire.collegeem.qc.ca>
2015-05-06 15:48:44 -0400

    Revert "Add script modify"
    This reverts commit 17daff8c8f78a6bdd56edf5a40760c92e8203e11.

---
+++ photo_hdr_duval_BD/2_S2_createStatut - Copie.sql
@@ -0,0 +1,29 @@
+USE [H15_PROJET_E05]
+GO
+
+--Statut
+------------------------------------------------------------------- --DROP TABLE RDV.Statut
+
+CREATE TABLE RDV.Statut(
+	StatutID INT NOT NULL IDENTITY,
+	DateModification DATETIME NOT NULL DEFAULT(GETDATE()), 
+	DescriptionStatut NVARCHAR(50) NOT NULL,
+	Statut INT NOT NULL,
+	RDVID INT NOT NULL
+
+	PRIMARY KEY (StatutID)
+) ON [PRIMARY];
+
+
+--Foreign Keys
+-------------------------------------------------------------------
+
+ALTER TABLE RDV.Statut
+	ADD CONSTRAINT FK_RDV_Statut_StatutID
+	FOREIGN KEY (StatutID)
+	REFERENCES RDV.RDVs
+
+--DONNÉES DE LA TABLE RDV.STATUT
+-------------------------------------------------------------------
+
+--INSERT INTO RDV.Statut(DescriptionStatut) VALUES ('Demandée'), ('Confirmée'), ('Reportée'), ('Réalisée'), ('Livrée'), ('Facturée')
