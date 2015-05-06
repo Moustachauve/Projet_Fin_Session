a09b5f42d44328effd16f11fcfa2b641aa940be6
IceSentry <c.giguere42@gmail.com>
2015-05-06 13:31:55 -0400

    Add forfaitMetadata

---
+++ photo_hdr_duval/photo_hdr_duval/Models/ForfaitMetaData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace photo_hdr_duval.Models
+{
+	[MetadataType(typeof(ForfaitMetaData))]
+	public partial class Forfait
+	{
+		internal sealed class ForfaitMetaData
+		{
+			
+			public int ForfaitID { get; set; }
+			[Display(Name = "Nom du forfait")]
+			public string Nom { get; set; }
+			[Display(Name = "Description du forfait")]
+			public string DescriptionForfait { get; set; }
+			[DisplayFormat(DataFormatString="${0:c2}")]
+			public decimal Prix { get; set; }
+		}
+	}
+}
