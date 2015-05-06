1e9dae843d0b9b1cf3efc20999d928cd3b4d553d
IceSentry <c.giguere42@gmail.com>
2015-05-06 16:56:30 -0400

    Revert "Update BD"
    This reverts commit 2882c158c95ed987d96249a379b12d6890eac9cf.

--- photo_hdr_duval_BD/INSERT.sql
+++ photo_hdr_duval_BD/INSERT.sql
@@ -1,19 +1,20 @@
 USE [H15_PROJET_E05]
 GO
 
---RDV.forfaits
-INSERT INTO RDV.Forfaits
-VALUES('Bronze', '20 photos et 35 minutes', 90),
-('Argent', '26 photos et 50 mintues', 105),
-('Or', '32 photos et 70 mintues', 120),
-('Personnalisé', 'À la discretion du photographe', 0)
-
 --RDV.RDVs
 
 INSERT INTO RDV.RDVs VALUES
 (GETDATE(), NULL, NULL, 'Appeller telephone secondaire avant 17h', 'Leduc, Eric', 4501231234, 0, '4321 Pas la vrai rue', 'Longueuil', 'EricCoolFriend@hotmail.com', 2),
 (GETDATE(), '2015-06-06', '15:10', 'N/A', 'Fafard, Pamela', 4501231235, 5141231234, '123 Sesame Street', 'Saint-Amable', 'unEmail@email.com', 1),
 (GETDATE(), NULL, NULL, 'N/A', 'Bastien, Ève', 4501231236, 0, 'rue Principal', 'Saint-Amable', 'unAutreEmail@email.com', 3)
+
+
+--RDV.forfaits
+INSERT INTO RDV.Forfaits
+VALUES('Bronze', '20 photos et 35 minutes', 90),
+('Argent', '26 photos et 50 mintues', 105),
+('Or', '32 photos et 70 mintues', 120),
+('Personnalisé', 'À la discretion du photographe', 0)
 
 --Agent.Agent
 INSERT INTO Agent.Agent VALUES
