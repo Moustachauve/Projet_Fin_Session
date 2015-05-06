b8318287a5face267046072bc8eccf169b028724
tigie11 <1369599@LLBD0605INFO08.laboratoire.collegeem.qc.ca>
2015-05-06 12:18:17 -0400

    modify gererStatut

---
+++ photo_hdr_duval_BD/ADD_01_Statut.sql
@@ -0,0 +1,26 @@
+USE [H15_PROJET_E05]
+GO
+
+--Statut
+------------------------------------------------------------------- --DROP TABLE RDV.Statut
+
+CREATE TABLE RDV.Statut(
+	StatutID INT NOT NULL IDENTITY,
+	DescriptionStatut NVARCHAR(50) NOT NULL UNIQUE,
+
+	PRIMARY KEY (StatutID)
+) ON [PRIMARY];
+
+
+--Foreign Keys
+-------------------------------------------------------------------
+
+ALTER TABLE RDV.RDVs
+	ADD CONSTRAINT FK_RDV_Statut_StatutID
+	FOREIGN KEY (StatutID)
+	REFERENCES RDV.Statut
+
+--DONNÉES DE LA TABLE RDV.STATUT
+-------------------------------------------------------------------
+
+INSERT INTO RDV.Statut(DescriptionStatut) VALUES ('Demandée'), ('Confirmée'), ('Reportée'), ('Réalisée'), ('Livrée'), ('Facturée')
