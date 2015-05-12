6370497eee4c776260f0f9b6a10daced538662f3
moustachauve <admin@cgagnier.ca>
2015-05-12 17:10:55 -0400

    Add JS library for gallery

---
+++ photo_hdr_duval/photo_hdr_duval/DAL/PhotoProprieteRepository.cs
@@ -0,0 +1,15 @@
+﻿using photo_hdr_duval.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace photo_hdr_duval.DAL
+{
+    public class PhotoProprieteRepository : Repository<PhotoPropriete>
+    {
+        public PhotoProprieteRepository(H15_PROJET_E05_Context context)
+            : base(context)
+        { }
+    }
+}
