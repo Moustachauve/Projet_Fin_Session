c7be0d83642bd1380085633baa46af8372fe8676
Christophe Gagnier <admin@cgagnier.ca>
2015-05-19 13:56:01 -0400

    Add PhotoProprieteMetaData

---
+++ photo_hdr_duval/photo_hdr_duval/Models/PhotoProprieteMetaData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace photo_hdr_duval.Models
+{
+    [MetadataType(typeof(PhotoProprieteMetaData))]
+    public partial class PhotoPropriete
+    {
+        internal sealed class PhotoProprieteMetaData
+        {
+            [DataType(DataType.MultilineText)]
+            [StringLength(300)]
+            public string DescriptionPhoto { get; set; }
+        }
+    }
+}
