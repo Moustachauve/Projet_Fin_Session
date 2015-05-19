6d9fb38bda7c771b425cbf615f705da4f3cb9b4e
Christophe Gagnier <admin@cgagnier.ca>
2015-05-19 15:02:49 -0400

    Start splitting photo methods in its own controller

---
+++ photo_hdr_duval/photo_hdr_duval/Controllers/PhotoProprietesController.cs
@@ -0,0 +1,151 @@
+﻿using photo_hdr_duval.DAL;
+using photo_hdr_duval.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+
+namespace photo_hdr_duval.Controllers
+{
+    public class PhotoProprietesController : Controller
+    {
+        private UnitOfWork uow = new UnitOfWork();
+
+        public ActionResult UploadPhoto(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            RDV rDV = uow.RDVRepository.GetByID((int)id);
+            return View(rDV);
+        }
+
+        public ActionResult EditPhoto(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhotoPropriete photo = uow.PhotoProprieteRepository.GetByID((int)id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(photo);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditPhoto(PhotoPropriete photo)
+        {
+            if (ModelState.IsValid)
+            {
+                PhotoPropriete vraiPhoto = uow.PhotoProprieteRepository.GetByID(photo.PhotoProprieteID);
+                vraiPhoto.DescriptionPhoto = photo.DescriptionPhoto;
+
+                uow.PhotoProprieteRepository.Update(vraiPhoto);
+                uow.Save();
+                return RedirectToAction("Details", new { id = vraiPhoto.RDVID });
+            }
+            return View(photo);
+        }
+
+        public ActionResult DownloadPhoto(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PhotoPropriete photo = uow.PhotoProprieteRepository.GetByID((int)id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+
+            string path = Server.MapPath(photo.Url);
+            string mime = MimeMapping.GetMimeMapping(path);
+
+            string date = ((DateTime)photo.RDV.DateRDV).ToShortDateString();
+            string ext = System.IO.Path.GetExtension(path);
+
+            return File(path, mime, date + "_" + photo.PhotoProprieteID + ext);
+        }
+
+        public JsonResult DoUploadPhoto(int? id)
+        {
+            if (id == null)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json("No ID");
+            }
+            if (Request.Files == null)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json("No file");
+            }
+
+            DirectoryInfo imageFolderPath = createImageRepo((int)id);
+
+            foreach (string fileString in Request.Files)
+            {
+                HttpPostedFileBase file = Request.Files[fileString];
+
+                string filename = Guid.NewGuid().ToString() + System.IO.Path.GetExtension(file.FileName);
+
+                file.SaveAs(imageFolderPath + "/" + filename);
+                string fullPath = "~/Images/" + id + "/" + filename;
+                uow.PhotoProprieteRepository.Insert(new PhotoPropriete() { Url = fullPath, RDVID = (int)id });
+            }
+
+            uow.Save();
+
+            return Json("File uploaded successfully");
+        }
+
+        public JsonResult DoDeletePhoto(int? id)
+        {
+            if (id == null)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json("No ID");
+            }
+
+            PhotoPropriete photo = uow.PhotoProprieteRepository.GetByID(id);
+            String path = HttpContext.Server.MapPath("~/images/" + photo.Url);
+            if (System.IO.File.Exists(path))
+                System.IO.File.Delete(path);
+
+            uow.PhotoProprieteRepository.Delete(photo);
+            uow.Save();
+
+            return Json(id);
+        }
+
+
+        private DirectoryInfo createImageRepo(int id)
+        {
+            DirectoryInfo imageFolderPath = new DirectoryInfo(Server.MapPath("~/Images/") + id);
+
+            if (!imageFolderPath.Exists)
+            {
+                DirectorySecurity securityRules = new DirectorySecurity();
+                SecurityIdentifier everyone = new SecurityIdentifier(WellKnownSidType.WorldSid, null);
+
+                securityRules.AddAccessRule(new FileSystemAccessRule(everyone, FileSystemRights.Modify | FileSystemRights.Synchronize,
+                                            InheritanceFlags.ContainerInherit | InheritanceFlags.ObjectInherit,
+                                            PropagationFlags.None, AccessControlType.Allow));
+
+                imageFolderPath.Create(securityRules);
+            }
+
+            return imageFolderPath;
+        }
+
+    }
+}
